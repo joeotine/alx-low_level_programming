71d57ddbc28950ed8a05b2a0ae68cf9edfa40253
Joseph Otine <joeotine@gmail.com>
2022-12-15 11:14:08 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,13 +7,8 @@
  * Return: 0
  *
  */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
