2f59a955c1dac5e67f19042ba74eb39eb9704bb3
Joseph Otine <joeotine@gmail.com>
2022-12-16 06:02:05 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry
- * Return: 1
+ * Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful/" - Dora Korpar, 2015-1019\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-1019\n", 59);
 	return (1);
 }
