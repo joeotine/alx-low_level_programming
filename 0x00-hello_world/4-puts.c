34ccf9a6d104a6d86f98d5b0580eaf7dfef0e435
Joseph Otine <joeotine@gmail.com>
2022-12-16 01:55:00 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  *
- * main - print a line of code using puts
+ * main - Description: print a line of code using puts
  *
  * Return: 0
  *
@@ -9,6 +9,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
