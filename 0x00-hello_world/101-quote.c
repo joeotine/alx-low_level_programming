071e1b066c4f4aa87a65d028c33500b4d9e1b94d
Joseph Otine <joeotine@gmail.com>
2022-12-16 05:53:33 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful/"\" - Dora Korpar, 2015-1019\n", 59);
+	write(1, "and that piece of art is useful/" - Dora Korpar, 2015-1019\n", 59);
 	return (1);
 }
