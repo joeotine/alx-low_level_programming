73ba45f1d44e87ca695bc4438c7efe9e7c4d2e0e
Joseph Otine <joeotine@gmail.com>
2022-12-16 05:40:23 -0800

    task 8

---
+++ 0x00-hello_world/100-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-1019\n", 59);
+	return (1);
+}
