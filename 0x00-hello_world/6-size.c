4f6221ad3a01cedbec94e55793bbb0bdcef8f81b
Joseph Otine <joeotine@gmail.com>
2022-12-16 03:44:47 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(ab));
+	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d))
 	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
