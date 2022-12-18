e7b1a7ef9ed0a153983c87e5b4da19c98a78c91e
Joseph Otine <joeotine@gmail.com>
2022-12-18 05:46:26 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,15 +6,15 @@
 void jack_bauer(void)
 {
 	int i, j;
-	
+
 	i = 0;
-	
+
 	while (i < 24)
-	
+
 	{
 		j = 0;
 		while (j < 60)
-		
+
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
