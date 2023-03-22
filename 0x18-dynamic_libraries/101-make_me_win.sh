5bfea97b10808cb8631a629265e750d156c745ab
Joseph Otine <joeotine@gmail.com>
2023-03-22 01:51:08 -0700

    C - Dynamic libraries tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/joeotine/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/path/to/nrandom.so
+wget -P /tmp/ https://raw.github.com/joeotine/alx-low_level_programming/master/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD=/path/to/libdynamic.so
